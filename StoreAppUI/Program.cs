c650c019fb2ace53dc8320c682186d62d55c664c
Satyam Rawalji <srawalji1@gmail.com>
2021-07-07 11:58:57 -0700

    First commit

---
+++ StoreAppUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+// dotnet new console -o StoreAppUI (creates project)
+namespace StoreAppUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
